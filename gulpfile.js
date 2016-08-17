b76b463d40e81806025eafac05247a75e87bd2e5
jws121295 <jws121295@gmail.com>
2016-08-17 19:53:54 -0400

    Refactor folder structuring

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gutil = require('gulp-util');
 var minify = require('gulp-minifier');
 
 var EXPRESS_PORT = 4001;
-var EXPRESS_ROOT = __dirname;
+var EXPRESS_ROOT = __dirname + '/public';
 var LIVERELOAD_PORT = 35729;
 
 
@@ -60,7 +60,7 @@ gulp.task('pug', function(){
     .pipe(pug({
        pretty: true
     }).on('error', gutil.log))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./public'))
 });
 
 gulp.task('watch', function(){
@@ -71,7 +71,7 @@ gulp.task('watch', function(){
 gulp.task('sass', function(){
     return gulp.src(['sass/app.scss'])
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('css/'));
+    .pipe(gulp.dest('./public/res/css/'));
 });
 
 gulp.task('produce', function(){
