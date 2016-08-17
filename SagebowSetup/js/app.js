1104b02220c752d0834eef9fd92d1d1e221df26a
Austin Fell <austinrf2918@yahoo.com>
2016-08-17 12:09:54 -0400

    Generated more pages, added more docs

--- SagebowSetup/js/app.js
+++ SagebowSetup/js/app.js
@@ -65,7 +65,17 @@ var modalGenerator = function( modalClassName, btnClassName ) {
 
 
 $(document).ready(function(){
-    var displayModal = modalGenerator( "modal-internal-error", "btn-internal-error-close" );
+    
+    //For when a user already exists.
+    //var displayModal = modalGenerator( "modal-user-exists", "btn-internal-user-exists" );
+
+    //For when an internal server error was encountered.
+    //var displayModal = modalGenerator( "modal-internal-error", "btn-internal-error-close" );
+
+
+    //For when user has not entered complete data (maybe call this from the front end?
+    var displayModal = modalGenerator( "modal-incomplete-data", "btn-incomplete-data-close" );
+
     displayModal.display();
 });
 
