55fcf4fbc15353f618ac78c068189d2fba31ab1c
Austin <austinrf2918@yahoo.com>
2016-11-19 17:24:19 -0500

    Componentalized modal a bit.

--- public/res/js/common.js
+++ public/res/js/common.js
@@ -7,44 +7,6 @@ function validateRequired(){
 	return inputsSatisfied && selectionsSatisfied;
 }
 
-/*globals $*/
-var FingModal = function(headerTxt, messageTxt, danger) {
-    var textClass = danger ? 'danger-text' : 'success-text';
-    var buttonClass = danger ? 'btn-reject' : 'btn-success';
-    
-    this.header = $('<h2>').addClass('m-b-1 ' +textClass).text(headerTxt);
-    this.message = $('<p>').text(messageTxt);
-    this.button = $('<a>').addClass('btn '+buttonClass).text('Close');
-    
-    this.el = $('<div>').addClass('modal hidden').append(
-        $('<div>').addClass('overlay').append(
-            $('<div>').addClass('window').append(
-                $('<div>').addClass('top-text')
-                .append(this.header)
-                .append(this.message)
-            ).append(
-                $('<div>').addClass('bottom-text text-xs-right').append(this.button)
-            )
-        )
-    );
-
-    this.show = function() {
-        $(document.body).append(this.el);
-        this.el.removeClass('hidden');
-    };
-
-    this.hide = function() {
-        this.el.addClass('hidden');
-        $(this.el).remove();
-    };
-
-    var self = this;
-    this.button.click(function() {
-        self.hide();
-    });
-};
-
-
 // Replace selects with dropdowns
 $(document).ready(function() {
     var currentZIndex = 9999;
--- public/res/js/modal-generator.js
+++ public/res/js/modal-generator.js
@@ -1,86 +1,36 @@
-/*
- * Constructor for a Modal, which via the display
- * method allows a modal to be displayed on document startup.
- * @constructor
- * @param {string} primaryClassName - the name of the modal
- * window. 
- * @param {string} btnClassName - the class name of the actual
- * button contained within our modal window.
- * @example
- * var displayModal = modalGenerator( "modal-internal-error", "btn-internal-error-close" );
- * displayModal.display();
- *
-*/
-var Modal = function( modalClassName, btnClassName ) {
-    return (function() {
-	var _localModalClassName = modalClassName;
-	var _localBtnClassName = btnClassName;
-	var _checkToggled = false;
+/*globals $*/
+var FingModal = function(headerTxt, messageTxt, danger) {
+    var textClass = danger ? 'danger-text' : 'success-text';
+    var buttonClass = danger ? 'btn-reject' : 'btn-success';
+    
+    this.header = $('<h2>').addClass('m-b-1 ' +textClass).text(headerTxt);
+    this.message = $('<p>').text(messageTxt);
+    this.button = $('<a>').addClass('btn '+buttonClass).text('Close');
+    
+    this.el = $('<div>').addClass('modal hidden').append(
+        $('<div>').addClass('overlay').append(
+            $('<div>').addClass('window').append(
+                $('<div>').addClass('top-text')
+                .append(this.header)
+                .append(this.message)
+            ).append(
+                $('<div>').addClass('bottom-text text-xs-right').append(this.button)
+            )
+        )
+    );
 
-	/*
-	* Internal function for our ModalGenerator, generates the 
-	* hidden class which is simpily the primary class name with
-	* hidden appended to it. 
-	* @method
-	* @param {string} primaryClassName - the name of the modal
-	* window which we will be hiding. 
-	*/
-	var _generateHiddenClass = function( primaryClassName ) {
-	    return (primaryClassName + "-hidden");
-	};
+    this.show = function() {
+        $(document.body).append(this.el);
+        this.el.removeClass('hidden');
+    };
 
-	/*
-	* Internal function for our ModalGenerator, sets checkToggled
-	* as false again to allow multiple refirings: this is to avoid
-	* the use of the display method multiple times which results in
-        * bugs.
-	* @method
-	* @param {string} primaryClassName - the name of the modal
-	* window which we will be hiding. 
-	*/
-	var _rearm = function( ) {
-	    _checkToggled = false;
-	};
+    this.hide = function() {
+        this.el.addClass('hidden');
+        $(this.el).remove();
+    };
 
-	/*
-	* Internal function for our ModalGenerator, generates the 
-	* toggleable button on a modal that allows it to be turned off.
-	* @method
-	* @param {string} primaryClassName - the name of the modal
-	* window. 
-	* @param {string} btnClassName - the class name of the actual
-	* button contained within our modal window.
-	*/
-	var _buildToggleableUnit = function( primaryClassName, btnClassName ) {
-	    $( "." + btnClassName ).on('click', function(){
-		$( "." + primaryClassName ).addClass( _generateHiddenClass(primaryClassName) );
-	    });
-	};
-
-	/*
-	* Internal function for our ModalGenerator, automatically the 
-	* shows a modal which is by default hidden.
-	* @method
-	* @param {string} primaryClassName - the name of the modal
-	* window. 
-	* @param {string} btnClassName - the class name of the actual
-	* button contained within our modal window.
-	*/
-	var displayModalOnView = function ( ) {
-	    //See: rearm to understand why I use this checkToggled parameter.
-	    if (_checkToggled == false) {
-	      $("." + _localModalClassName).removeClass( _generateHiddenClass(_localModalClassName));
-	      _buildToggleableUnit( _localModalClassName, _localBtnClassName);
-	      _checkToggled = true;
-	    } else {
-		throw "A modal window was already created: please call the rearm method to allow another toggle.";
-	    }
-	};
-
-	return {
-	    display: displayModalOnView,
-	    rearm: _rearm
-	};
-    })();
+    var self = this;
+    this.button.click(function() {
+        self.hide();
+    });
 };
-
