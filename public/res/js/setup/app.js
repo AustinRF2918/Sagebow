57419b74c4835102c4b75983723d3ec279206947
Austin <austinrf2918@yahoo.com>
2016-11-21 03:17:33 -0500

    Seperated business logic: needs comments.

--- public/res/js/setup/app.js
+++ public/res/js/setup/app.js
@@ -1,52 +1,5 @@
 /*globals $, Dropdown, Modal*/
 
-function checkInputs() {
-	
-}
-
-// Create a general business logic library.
-function toInches(feet) {
-	return feet * 12;
-}
-
-function calculateBmi(weight, height) {
-	//BMI = ( Weight in Pounds / ( Height in inches x Height in inches ) ) x 703
-	return (weight / (toInches(height) * toInches(height))) * 703;
-}
-
-function calculateBmr(weight, height, gender, age) {
-	if (gender === 'Male') {
-		return 655 + (4.35 * weight) + (4.7 * toInches(height)) - (4.7 * age);
-	}
-	else if (gender === 'Female') {
-		return 66 + (6.23 * weight) + (12.7 * toInches(height)) - (6.8 * age);
-	}
-	//     BMR for men: 655 + (4.35 x weight in pounds) + (4.7 x height in inches) - (4.7 x age in years)
-	// BMR for women: 66 + (6.23 x weight in pounds) + (12.7 x height in inches) - (6.8 x age in years)
-	// multiply your BMR by an activity factor to determine your caloric needs. Each activity category listed below has a range. Choose a number based on where you fall in that range.
-}
-
-function calculateDailyCalories(bmr, activityLevel, goal) {
-	var activityMap = {
-		'sedentary': bmr * 1.39,
-		'lightly': bmr * 1.59,
-		'moderately': bmr * 1.89,
-		'very': bmr * 2.5
-	};
-	var goalAdj = 1;
-	if(goal === "Weight Loss"){
-		goalAdj = 0.8;
-	}
-	if(goal === "Bodybuilding"){
-		goalAdj = 1.2;
-	}
-	return activityMap[activityLevel.split(' ')[0].toLowerCase()] * goalAdj;
-	// If you are sedentary or mostly sedentary multiply your BMR by 1.0-1.39 
-	// If you are lightly active (you do 30-60 minutes of easy physical activity each day), multiply your BMR by 1.4-1.59
-	// If you are moderately active (you do 60 minutes of moderate physical activity each day) multiply your BMR by 1.6-1.89
-	// If you are very active multiply your BMR by 1.9-2.5. Very active people do at least 60 minutes of moderate physical activity each day plus 60 minutes of vigorous activity or do at least 120 minutes of moderate activity each day.
-}
-
 $(document).ready(function() {
     var UserModel = Backbone.Model.extend({
 	defaults: {
@@ -104,8 +57,6 @@ $(document).ready(function() {
 		    return item.value === '';
 	    }).length === 0;
 
-	    console.log($('#gender').attr('set'));
-
 	    return inputsSatisfied &&
 		   $('#gender').attr('set') === "true" &&
 		   $('#goal').attr('set') === "true" &&
@@ -140,6 +91,7 @@ $(document).ready(function() {
 	},
 
 	attemptCreation: function(event) {
+	    console.log("Hi.");
 	    if (event.keyCode && event.keyCode !== 13 ) {
 		return;
 	    } else {
