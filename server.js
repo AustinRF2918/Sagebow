7dfc2fc19d28c038dc5892a77d1e6db8cc7ccc1b
Austin <austinrf2918@yahoo.com>
2016-11-25 20:08:32 -0500

    Modification of server code, more ES6.

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ module.exports = function(EXPRESS_PORT, EXPRESS_ROOT) {
 
 	// Attempt to retrieve a username from the redis
 	// cache: this may have multiple outcomes.
-        redisConn.get(username, function(err, userObj) {
+        redisConn.get(username, (err, userObj) => {
             userObj = JSON.parse(userObj);
 
             if (err) {
@@ -253,53 +253,110 @@ module.exports = function(EXPRESS_PORT, EXPRESS_ROOT) {
     // End of public routes.
 
     // PRIVATE ROUTES
+
+    // * path
+    //
+    // In the case that neither login or setup were called, we use
+    // this wildstar pattern to check for the existance of a user
+    // session object.
     app.get('*', function(req, res, next) {
         if (req.session.userObj) {
-            // Free to proceed
+	    // In the case that our session contains a user object,
+	    // continue on to the next routes.
             next();
-        }
-        else {
-            // Login first!
-            res.redirect('/login#noAccess');
+        } else {
+	    // Otherwise, signify that the user must login by redirecting
+	    // to the login page.
+	    to the login page.
+            res.redirect('/login');
         }
     });
 
+    // Logout/Login endpoint
+    //
+    // This is a simple "endpoint", though not exactly an endpoint. 
+    // It deletes the current user object and redirects the client
+    // to the login page.
     app.get(/^\/log(out|off)$/,function(req,res){
         delete req.session.userObj;
         res.redirect('/login');
     });
     
+    // Setup Static Serve
+    //
+    // Serves the static markup for the Delete page.
     app.get('/delete',function(req,res){
         serveFile('delete.html',res);
     });
-    
-    // Post Delete
+
+    // Delete endpoint
+    //
+    // This is a simple "endpoint", though not exactly an endpoint. 
+    // It deletes the current user object and redirects the client
+    // to the login page.
     app.post('/delete', function(req, res) {
-        var username = req.body.username.trim();
-        var password = req.body.password.trim();
+	const neededFields = new Set([
+	    'username',
+            'password'
+	]);
+
+	// A mapping of all required fields onto the request body:
+	// in the case that all objects are mapped, the filter
+	// later on will create an array of length zero, indicating
+	// that all of our data was on the body of the request, otherwise,
+	// an error will be indicated.
+	const fields = Array.from(neededFields.values())
+	    .map((item) => req.body[item])
+	    .filter((item) => item === null || item === undefined);
+
+
+	// Make sure all of our fields were filtered out.
+	// Otherwise send a malformed error code.
+	if (fields.length !== 0) {
+	    res.status(422).send('Malformed');
+	    return;
+	}
+
+        const username = req.body.username.trim();
+        const password = req.body.password.trim();
 
         // Attempt user lookup
-        redisConn.get(username, function(err, userObj) {
+	redisConn.get(username, (err, userObj) => {
             userObj = JSON.parse(userObj);
 
-            if (err)
-                res.send('error');
-            else if (!userObj)
-                res.send('invalid');
-            else if (!bcrypt.compareSync(password, userObj.passwordHash))
-                res.send('invalid');
-            else {
+            if (err) {
+		// Send a 500 internal server error in the
+		// case that some error was thrown on trying
+		// to connect to the Redis database.
+		res.status(500).send('Error');
+	    } else if (!userObj) {
+		// Send a not found in the case that no user
+		// object is found for the cooresponding username.
+		res.status(404).send('Not Found');
+	    } else if (!bcrypt.compareSync(password, userObj.passwordHash)) {
+		// Send a malformed in the case that the password
+		// doesn't quite match up.
+		res.status(422).send('Malformed');
+	    } else {
+		// If none of these cases have happened, create a user object
+		// of the specific client that is attempting to log in and
+		// send a 200 status code.
                 redisConn.del(username);
-                console.log('delete account');
-                res.send('success');
+                res.sendStatus(200);
             }
         });
     });
 
+    // Metrics Static Serve
+    //
+    // Serves the static markup for the Metrics page.
     app.get('/metrics', function(req, res) {
         serveFile('metrics.html', res);
     });
 
+    // Entry Static Serve
+    //
+    // Serves the static markup for the Entry page.
     app.get('/entry', function(req, res) {
         serveFile('entry.html', res);
     });
