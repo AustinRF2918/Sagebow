81ac937ff864be20741542e9dd3ca8a825759cf2
Austin <austinrf2918@yahoo.com>
2016-11-29 00:42:28 -0500

    More testing.

--- test/user_facing/login.py
+++ test/user_facing/login.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+
 
 driver = webdriver.Chrome('/usr/bin/chromedriver')
 driver.set_window_size(1120, 1120)
@@ -129,9 +131,83 @@ class TestIntroduction(unittest.TestCase):
 
         self.assertEqual('Oops' in driver.page_source, True)
 
-    
+    def test_modal_open_enter(self):
+        driver.get("http://localhost:4001/login")
+        name = driver.find_element_by_id('username')
+        password = driver.find_element_by_id('password')
+        login = driver.find_element_by_class_name('btn-login')
+        page = driver.find_element_by_class_name('full-container')
+        account_creation = driver.find_element_by_class_name('btn-make-account')
+
+        name.send_keys("boston");
+        password.send_keys("gdsagdsagjkdksagjdk");
+
+        password.send_keys(Keys.ENTER);
+        time.sleep(0.8)
+        self.assertEqual('The password you entered is incorrect.' in driver.page_source, True)
+        driver.find_element_by_class_name('btn-reject').click()
+        time.sleep(0.8)
+
+        name.send_keys(Keys.ENTER);
+        time.sleep(0.8)
+        self.assertEqual('The password you entered is incorrect.' in driver.page_source, True)
+        driver.find_element_by_class_name('btn-reject').click()
+        time.sleep(0.8)
+
+        login.send_keys(Keys.ENTER);
+        time.sleep(0.8)
+        self.assertEqual('The password you entered is incorrect.' in driver.page_source, True)
+        driver.find_element_by_class_name('btn-reject').click()
+        time.sleep(0.8)
+
+        account_creation.send_keys(Keys.ENTER);
+        time.sleep(0.8)
+        self.assertEqual('Sign Up' in driver.page_source, True)
+        time.sleep(0.8)
+
+    def test_modal_close_enter_from_enter(self):
+        driver.get("http://localhost:4001/login")
+        name = driver.find_element_by_id('username')
+        password = driver.find_element_by_id('password')
+        login = driver.find_element_by_class_name('btn-login')
+        page = driver.find_element_by_class_name('full-container')
+        account_creation = driver.find_element_by_class_name('btn-make-account')
+
+        name.send_keys("boston");
+        password.send_keys("gdsagdsagjkdksagjdk");
+        password.send_keys(Keys.ENTER);
+
+        time.sleep(.5)
+        self.assertEqual('The password you entered is incorrect.' in driver.page_source, True)
+        action = webdriver.common.action_chains.ActionChains(driver)
+        action.send_keys(Keys.ENTER)
+        action.perform()
+        self.assertEqual('The password you entered is incorrect.' in driver.page_source, False)
+        time.sleep(.5)
+
+    def test_modal_close_enter_from_click(self):
+        driver.get("http://localhost:4001/login")
+        name = driver.find_element_by_id('username')
+        password = driver.find_element_by_id('password')
+        login = driver.find_element_by_class_name('btn-login')
+        page = driver.find_element_by_class_name('full-container')
+        account_creation = driver.find_element_by_class_name('btn-make-account')
+
+        name.send_keys("boston");
+        password.send_keys("gdsagdsagjkdksagjdk");
+        driver.find_element_by_class_name('btn-login').click()
+
+        time.sleep(.5)
+        self.assertEqual('The password you entered is incorrect.' in driver.page_source, True)
+        action = webdriver.common.action_chains.ActionChains(driver)
+        action.send_keys(Keys.ENTER)
+        action.perform()
+        self.assertEqual('The password you entered is incorrect.' in driver.page_source, False)
+        time.sleep(.5)
+
     def tearDown(self):
         driver.get("http://localhost:4001/login")
+        time.sleep(0.1)
         
 
 
