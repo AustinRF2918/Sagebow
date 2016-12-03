bae6548ffe205bb05297476c86000d0cfb5a18e9
Austin Fell <austinrf2918@yahoo.com>
2016-12-03 03:24:46 -0500

    Began developing validateRequest to dry up code.

--- app/utilities/integrity.js
+++ app/utilities/integrity.js
@@ -1,22 +1,28 @@
-function checkFields(set, req) {
+function validateRequest(fields, req) {
     // A mapping of all required fields onto the request body:
     // in the case that all objects are mapped, the filter
     // later on will create an array of length zero, indicating
     // that all of our data was on the body of the request, otherwise,
     // an error will be indicated.
-    const fields = Array.from(set.values())
+    const mappedFields = fields
 	.map((item) => req.body[item])
 	.filter((item) => item === null || item === undefined);
 
     // Make sure all of our fields were filtered out.
     // Otherwise send a malformed error code.
-    if (fields.length !== 0) {
-	return false;
+    if (mappedFields.length !== 0) {
+	// Our request did not have all of the neccessary information.
+	return null;
     } else {
-	return true;
+	// Our request did have all the neccessary information and now
+	// will map over it.
+	return fields.reduce((total, current) => {
+	    total[current] = req.body[current];
+	    return total;
+	});
     }
 }
 
 module.exports = {
-    checkFields
+    validateRequest
 };
