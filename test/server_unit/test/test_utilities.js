9d3f0112f5e99f4e1b3af8d1be9fd0f0cdcf7423
Austin Fell <austinrf2918@yahoo.com>
2016-12-04 00:36:45 -0500

    Got tests up for request validation.

--- test/server_unit/test/test_utilities.js
+++ test/server_unit/test/test_utilities.js
@@ -22,4 +22,81 @@ describe('Integrity Checker', function() {
 	expect(data["password"]).to.equal("world");
 	expect(data["input"]).to.equal("test");
     });
+
+    it('Should not map variables that werent neccessary required.', function() {
+	var data = validateRequest(["username", "password", "input"], {
+	    params: {"input": "test", "output": "what"},
+	    body: {"username": "hello", "password": "world", "credential": "is"}
+	});
+
+	expect(data["username"]).to.equal("hello");
+	expect(data["password"]).to.equal("world");
+	expect(data["input"]).to.equal("test");
+	expect(data["output"]).to.not.equal("what");
+	expect(data["credential"]).to.not.equal("is");
+    });
+
+    it('Should not map variables that werent neccessary required body.', function() {
+	var data = validateRequest(["username", "password", "input"], {
+	    params: {"input": "test", "output": "what", "input": "what"},
+	    body: {"password": "world", "credential": "is"}
+	}, {
+	    status: function(param) {
+		return {
+		    send: function(param) {
+		    }
+		};
+	    }
+	});
+
+	expect(data).to.equal(null);
+    });
+
+    it('Should not map variables that werent neccessary required params.', function() {
+	var data = validateRequest(["username", "password", "input"], {
+	    params: {"output": "what"},
+	    body: {username: "hello", "password": "world", "credential": "is"}
+	}, {
+	    status: function(param) {
+		return {
+		    send: function(param) {
+		    }
+		};
+	    }
+	});
+
+	expect(data).to.equal(null);
+    });
+
+    it('Should not map variables that werent neccessary required both.', function() {
+	var data = validateRequest(["username", "password", "input"], {
+	    params: {"output": "what"},
+	    body: {"password": "world", "credential": "is"}
+	}, {
+	    status: function(param) {
+		return {
+		    send: function(param) {
+		    }
+		};
+	    }
+	});
+
+	expect(data).to.equal(null);
+    });
+
+    it('Should pass back empty object if no parameters required.', function() {
+	var data = validateRequest([], {
+	    params: {"output": "what"},
+	    body: {"password": "world", "credential": "is"}
+	}, {
+	    status: function(param) {
+		return {
+		    send: function(param) {
+		    }
+		};
+	    }
+	});
+
+	expect(data).to.deep.equal({});
+    });
 });
