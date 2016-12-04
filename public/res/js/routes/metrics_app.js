b4cf059c96982e6e5f7beeceee41fae66b628dd4
Austin Fell <austinrf2918@yahoo.com>
2016-12-04 03:08:49 -0500

    Got ready for more orginized client side routing.

---
+++ public/res/js/routes/metrics_app.js
@@ -0,0 +1,194 @@
+console.log("Hello");
+
+/*globals $, Chart, moment*/
+var consumptionData = [];
+var currentNutrient = "calories";
+var currentTimespan = "week";
+
+function reduce(array, operation, initial){
+	var finalResult = initial;
+	for(var element of array){
+		finalResult = operation(finalResult,element);
+	}
+	return finalResult;
+};
+
+var FingBarGraph = function($ctx, headerTxt, days, data) {
+	$('#data-plot canvas').remove();
+	$('#data-plot').append($('<canvas width="345" height="172" style="width: 345px; height: 172px;">'));
+    return (new Chart($('#data-plot canvas'), {
+		type: 'bar',
+		data: {
+		    labels: days,
+		    datasets: [{
+			label: headerTxt,
+			data: data,
+			backgroundColor: '#B71C1C',
+			borderColor: 'rgba(0, 0, 0, 0.9)',
+			borderWidth: 1.5
+		    }]
+		},
+		options: {
+		    scales: {
+			yAxes: [{
+			    ticks: {
+				beginAtZero:true
+			    }
+			}]
+		    }
+		}
+    }));
+};
+
+var FingPieGraph = function($ctx, headerTxt, rationalNutrient) {
+    return (new Chart($ctx, {
+	type: 'pie',
+	data: {
+	    labels: ["Protein", "Carbs", "Fat"],
+	    datasets: [{
+		label: 'Current Calorie Intake',
+		data: [rationalNutrient.proteins, rationalNutrient.carbs, rationalNutrient.fats],
+		backgroundColor: ['#B71C1C',
+				  '#880E4F',
+				  '#4A148C'],
+		borderColor: 'rgba(255, 99, 132, 0.3)',
+		borderWidth: 1.5
+	    }]
+	},
+	options: {
+	    scales: {
+		yAxes: [{
+		    ticks: {
+			beginAtZero:true
+		    }
+		}]
+	    }
+	}
+    }));
+};
+
+function extractMetric(key, unit){
+	var min = moment().subtract(1, unit);
+	return consumptionData.reduce(function(previous, el){
+		var time = new Date(el.timestamp);
+		if(time >  min._d){
+			if(el[key]){
+				previous.push(el[key]);
+			}
+		}else{
+			console.log('DATE ESCAPE!');
+			console.log(range.min);
+			console.log(time);
+			console.log(range.max);
+		}
+		return previous;
+	},[]);
+};
+
+function _classFromValue(num) {
+	if (num >= 0 && num < 33.333) {
+		return "not-even-close";
+	} else if (num >= 33.333 && num < 66.666) {
+		return "middle-close";
+	} else {
+		return "getting-close";
+	}
+};
+
+function _applyClassToMeter($jObj) {
+	$jObj.addClass(_classFromValue($jObj.attr("value")));
+}
+
+function stylizeBars() {
+	_applyClassToMeter(($("#calories-goal")));
+	_applyClassToMeter(($("#proteins-goal")));
+	_applyClassToMeter(($("#fats-goal")));
+	_applyClassToMeter(($("#carbs-goal")));
+};
+
+function summation(pre,cur){
+	return pre+cur;
+};
+
+function updateBarGraph(){
+	if(currentNutrient !== 'weight'){
+		FingBarGraph($("#data-plot"), 
+			"Your " + currentNutrient + " Intake for the "+currentTimespan, 
+			extractMetric('timestamp', currentTimespan).map(function(ts){
+				return moment(ts).format('MMM DD');
+			}), 
+			extractMetric(currentNutrient, currentTimespan));
+	}else{
+		$.get('/api/weight').done(function(weightEvents){
+			var dates = weightEvents.map(function(el){
+				return moment(el.timestamp).format('MMM DD');
+			});
+			
+			var weights = weightEvents.map(function(el) {
+				return el.weight;
+			});
+			
+			FingBarGraph($('#data-plot'), "Your weight history for the " + currentTimespan, dates, weights);
+		});
+	}
+}
+
+$(document).ready(function() {
+    $.get('/api/consumption',function(consumptionEvents){
+    	consumptionData = consumptionEvents;
+    	
+    	updateBarGraph();
+    	
+    	if(consumptionData.length > 0){
+    		var proteins = extractMetric('proteins','day'),
+    			carbs = extractMetric('carbs','day'),
+    			fats = extractMetric('fats','day');
+    			
+	    	var counts = new NutrientRatio(
+	    		proteins.reduce(summation,0),
+	    		carbs.reduce(summation,0),
+	    		fats.reduce(summation,0));
+	    	FingPieGraph($("#consumption-plot"), "Daily Caloric intake", counts);
+	    	$.get('/api/diet').done(function(diet){
+		    	var nutrients = new NutrientRatio(diet.dailyCalories, diet.goal);
+	    		console.log(counts.fats * 9);
+	    		console.log(nutrients.fats);
+		    	$("#calories-goal").val(100*counts.calories/nutrients.calories);
+			    $("#proteins-goal").val(100*(counts.proteins)/(nutrients.proteins / 4));
+			    $("#fats-goal").val(100*(counts.fats)/(nutrients.fats / 9));
+			    $("#carbs-goal").val(100*(counts.carbs)/(nutrients.carbs / 4));
+		    	stylizeBars();
+		    });
+    	}
+    });
+    
+    $(".dropdown-item").click(function(){
+    	switch (this.innerHTML) {
+    		case "Weekly": 
+    			currentTimespan = "week";
+    			break;
+    		case "Monthly": 
+    			currentTimespan = "month";
+    			break;
+    		case "Yearly": 
+    			currentTimespan = "year";
+    			break;
+    		case "Calories": 
+    			currentNutrient = "calories";
+    			break;
+    		case "Protein": 
+    			currentNutrient = "proteins";
+    			break;
+    		case "Fat": 
+    			currentNutrient = 'fats';
+    			break;
+    		case "Carbs": 
+    			currentNutrient = 'carbs';
+    			break;
+    		case "Weight":
+    			currentNutrient = 'weight';
+    			break;
+    	}
+		updateBarGraph();
+    });
+});
---
+++ public/res/js/routes/setup_app.js
@@ -0,0 +1,188 @@
+/*globals $, Dropdown, Modal*/
+
+var SetupView = Backbone.View.extend({
+    // The tag that represents the
+    // hook that this view is associated
+    // with.
+    el: $("body"),
+
+    // UI Components.
+    $username: undefined,
+    $password: undefined,
+    $genderSelector: undefined,
+    $goalSelector: undefined,
+    $activitySelector: undefined,
+    $weight: undefined,
+    $height: undefined,
+    $age: undefined,
+
+    // Models.
+    userFields: undefined,
+
+    initialize: function(attrs) {
+	this.el = this.options.applicationContainer;
+	this.options = attrs;
+
+	// The user model that will be passed back and
+	// fourth between the server.
+	this.userFields = this.options.user;
+
+	// The actual user interface fields (as JQuery
+	// selectors). We can use these to synchronize
+	// the user model introduced after this and
+	// send it to the server for analysis and
+	// creation.
+	this.$username = this.options.$username;
+	this.$password = this.options.$password;
+	this.$genderSelector = this.options.$genderSelector;
+	this.$goalSelector = this.options.$goalSelector;
+	this.$activitySelector = this.options.$activitySelector;
+	this.$weight = this.options.$weight;
+	this.$height = this.options.$height;
+	this.$age = this.options.$age;
+
+	// Stack of current modals.
+	this.modals = [];
+
+	_.bindAll(this, 'render');
+	this.render();
+
+	var that = this;
+
+	$(this.el).find(".btn-create").click(function(event) {
+	    $(".btn-create").blur();
+	    that.attemptCreation(event);
+	});
+
+	$(document).keypress(function(event) {
+	    if (event.which === 13 ) {
+		if (($('body').has('.window').length === 0)) {
+		    that.attemptCreation(event);
+		} 
+	    } 
+	});
+    },
+
+    // validateRequired() takes the global DOM and checks for all DOM
+    // nodes with the required class, following this it filters the
+    // nodes for ANY item that is not equal to ''. If any nodes are
+    // in the resultant list, our inputs will not be satisfied.
+    validateRequired: function() {
+	var fieldsFilled = $(this.el)
+	    .find('.required')
+	    .filter(function(element, item) {
+		return ($(item).attr('set') === 'false')|| item.value === '';
+	    });
+
+	return fieldsFilled.length === 0;
+    },
+
+    getFields: function() {
+	return {
+	    username: this.$username.val(),
+	    password: this.$password.val(),
+	    weight: Number(this.$weight.val()),
+	    height: Number(this.$height.val()),
+	    age: Number(this.$age.val()),
+	    bmi: calculateBmi(Number(this.$weight.val()), Number(this.$height.val())),
+	    bmr: calculateBmr(Number(this.$weight.val()), Number(this.$height.val()), this.$genderSelector.text(), Number(this.$age.val())),
+	    activitySelector: this.$activitySelector.text(),
+	    genderSelector: this.$genderSelector.text(),
+	    goalSelector: this.$goalSelector.text(),
+	    dailyCalories: calculateDailyCalories(
+		calculateBmr(Number(this.$weight.val()),
+				Number(this.$height.val()),
+				this.$genderSelector.text(),
+				Number(this.$age.val())),
+		this.$activitySelector.text(),
+		this.$goalSelector.text())
+	};
+    },
+
+    attemptCreation: function(event) {
+	if (!this.validateRequired()) {
+	    var modal = produceModal("Oops", "All fields are required.", true);
+	    modal.display($(this.el));
+	    console.log("HI");
+	    return false;
+	}
+
+
+	var fields = this.getFields();
+	var that = this;
+
+	// Save the model using this.getFields(): We could actually automatically
+	// update our model on form changes and we wouldn't even have to use
+	// the "this.getFields() functionality, and in fact we could fully delete
+	// it.
+	this.userFields.save(fields, {
+	    dataType: 'text',
+
+	    success: function(model, response) {
+		var successModal = new ModalView({
+		    header: "Nice",
+		    message: "You made an account! Press okay to go to the login page.",
+		    isDangerous: false,
+		    closeFn: function() {
+			window.location.href = '/login';
+		    }
+		});
+
+		successModal.display($(that.el));
+		that.currentModals.push(successModal);
+		return true;
+	    },
+
+	    error: function(model, response) {
+		var modal;
+
+		if (response.responseText === "Error") {
+		    modal = produceModal("Oops", "An error occured on our server, maybe you want to try again later?", true);
+		    modal.display($(that.el));
+		    that.currentModals.push(modal);
+		    return false;
+		} else if (response.responseText === "Conflict") {
+		    modal = produceModal("Oops", "A user with this account name already exists!", true);
+		    modal.display($(that.el));
+		    that.currentModals.push(modal);
+		    return false;
+		} else if (response.responseText === "Malformed") {
+		    modal = produceModal("Oops", "The data you sent us was malformed!", true);
+		    modal.display($(that.el));
+		    that.currentModals.push(modal);
+		    return false;
+		} else {
+		    modal = produceModal("Oops", "An unknown error occured on our server, maybe you want to try again later?", true);
+		    modal.display($(that.el));
+		    that.currentModals.push(modal);
+		    return false;
+		}
+	    }
+	});
+
+	return true;
+    }
+});
+
+$(document).ready(function() {
+    // Replace all the dropdowns on our application before we
+    // actually create the app view.
+    DropdownReplacer.replaceDropdowns($(".cta-container"));
+
+    // Create user object.
+    userObject = new UserModelSetup();
+
+    // Create actual page of application.
+    app = new SetupView({
+	applicationContainer: $("body"),
+	$username: $("#username"),
+	$password: $("#password"),
+	$genderSelector: $("#gender"),
+	$goalSelector: $("#goal"),
+	$activitySelector: $("#activity-level"),
+	$weight: $("#weight"),
+	$height: $("#height"),
+	$age: $("#age"),
+	user: userObject
+    });
+});
